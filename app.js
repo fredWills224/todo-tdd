83d63b913967d4067fa2f1f30735a5f37efd9b28
fredWills224 <fredwills4229@gmail.com>
2020-10-13 12:19:58 -0400

    first failing test

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.json("Hello world!");
+app.get('/', (req, res)=>{
+    res.json('Hello world!');
 });
 
 app.listen(3000, ()=>{
-    console.log("Server is now running!");
+    console.log('Server is now running!');
 });
