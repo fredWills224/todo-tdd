5f6a592e5d4e016e428c7da1fb8f70232b222407
fredWills224 <fredwills4229@gmail.com>
2020-10-14 10:03:14 -0400

    should return json body fail

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -14,15 +14,40 @@ beforeEach(()=>{
 
 describe('TodoController.createTodo', ()=>{
     
+    beforeEach(()=>{
+        req.body = newTodo;
+    });
+
     it('should have a createTodo function', ()=>{
         expect(typeof TodoController.createTodo).toBe('function');
     });
 
     it('should call TodoModel.create with req.body', ()=>{
-        req.body = newTodo;
         TodoController.createTodo(req, res, next);
         expect(TodoModel.create).toBeCalledWith(newTodo);
     });
 
+    it('should return 201 response code', ()=>{
+        TodoController.createTodo(req, res, next);
+        //when creating a new resource in rest a status of 201 should be returned
+        expect(res.statusCode).toBe(201);
+        //insure response has been sent back
+        expect(res._isEndCalled()).toBeTruthy();
+    });
+
+    it('should return json body in response',()=>{
+        TodoModel.create.mockReturnValue(newTodo);
+        TodoController.createTodo(req, res, next);
+        expect(res._getJSONData()).toBe(newTodo);        
+    });
+
 });
+
+
+
+
+
+
+
+
 
