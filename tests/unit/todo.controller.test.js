5472fad3f51f0c57ee2bea2ddeb00edf3e20cb28
fredWills224 <fredwills4229@gmail.com>
2020-10-16 12:35:31 -0400

    error handling test fail

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -9,7 +9,7 @@ let req, res, next;
 beforeEach(()=>{
     req = httpMocks.createRequest();
     res = httpMocks.createResponse();
-    next = null;
+    next = jest.fn();
 });
 
 describe('TodoController.createTodo', ()=>{
@@ -41,6 +41,14 @@ describe('TodoController.createTodo', ()=>{
         //[toStrictEqual()] is used insted of [toBe()] because [newTodo] and
         //[creatModel] have different reference numbers in memory
         expect(res._getJSONData()).toStrictEqual(newTodo);        
+    });
+
+    it('should handle errors', async ()=>{
+        const errorMessage = { message: "Done property missing" };
+        const rejectedPromise = Promise.reject(errorMessage);
+        TodoModel.create.mockReturnValue(rejectedPromise);
+        await TodoController.createTodo(req, res, next);
+        expect(next).toBeCalledWith(errorMessage); 
     });
 
 });
