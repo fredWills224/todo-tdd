6fcebd4c3f46b9e74c63c5f4784472ab19121be0
fredWills224 <fredwills4229@gmail.com>
2020-10-16 11:21:31 -0400

    express listening to diff ports in with diff files

--- app.js
+++ app.js
@@ -13,15 +13,4 @@ app.get('/', (req, res)=>{
     res.json('Hello world!');
 });
 
-// Comment out line 20 to satisfy
-// [A worker process has failed to exit gracefully
-// and has been force exited. This is likely caused
-// by tests leaking due to improper teardown.
-// Try running with --runInBand --detectOpenHandles to find leaks.]
-// warning 
-
-// app.listen(3000, ()=>{
-//     console.log('Server is now running!');
-// });
-
 module.exports = app;
