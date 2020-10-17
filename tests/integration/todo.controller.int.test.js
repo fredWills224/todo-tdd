2aeda6e5ee03a0a0cae114e088b7d3e75853fd89
fredWills224 <fredwills4229@gmail.com>
2020-10-17 16:28:00 -0400

    getTodoById int test fail + pass

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -3,7 +3,7 @@ const app = require('../../app');
 const newTodo = require('../mock-data/new-todo.json');
 
 const endpointUrl = '/todos/';
-
+let firstTodo;
 describe(endpointUrl, ()=>{
     
     beforeEach(()=>{
@@ -17,6 +17,15 @@ describe(endpointUrl, ()=>{
         expect(Array.isArray(response.body)).toBeTruthy();
         expect(response.body[0].title).toBeDefined();
         expect(response.body[0].done).toBeDefined();
+        firstTodo = response.body[0];
+
+    });
+
+    test('GET by Id ' + endpointUrl + ':todoId', async ()=>{
+        const response = await request(app).get(endpointUrl + firstTodo._id);
+        expect(response.statusCode).toBe(200);
+        expect(response.body.title).toBe(firstTodo.title);
+        expect(response.body.done).toBe(firstTodo.done);
 
     });
 
